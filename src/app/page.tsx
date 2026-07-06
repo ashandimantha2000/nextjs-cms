7c52ac9751c7c5f3b4f41d0ca8a612482caa0496
Ashan Dimantha <Ashan Dimantha>
2026-07-06 16:09:23 +0530

    chore: update home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import Link from 'next/link'
 
-export default function Home() {
+export default function page() {
   return (
     <div>
-      <h1>Welcome to Home</h1>
+      <h1>Welcome to NextJs - CMS Integrations</h1>
+      <h2>Ghost | Payload CMS | Strapi</h2>
+      <br />
+      <Link href="/blog" className='bg-blue-900 p-3 rounded-sm'>Visit Blogs Page</Link>
     </div>
-  )
+  );
 }
