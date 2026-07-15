6105acf19c08a6ebca74956048a833ef45f03030
Ashan Dimantha <Ashan Dimantha>
2026-07-15 15:46:31 +0530

    fix: HTTPS and image issues

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,14 @@
 import type { NextConfig } from "next";
 
+if (process.env.NODE_ENV === "development") {
+  process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+}
+
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
   images: {
+    dangerouslyAllowLocalIP: process.env.NODE_ENV === "development",
     remotePatterns: [
       {
         protocol: "http",
@@ -13,9 +18,24 @@ const nextConfig: NextConfig = {
       },
       {
         protocol: "https",
+        hostname: "localhost",
+        pathname: "/content/images/**",
+      },
+      {
+        protocol: "https",
         hostname: "static.ghost.org",
         pathname: "/**",
       },
+      {
+        protocol: "https",
+        hostname: "www.gravatar.com",
+        pathname: "/avatar/**",
+      },
+      {
+        protocol: "https",
+        hostname: "localhost",
+        pathname: "/**",
+      }
     ],
   },
 };
--- src/app/api/api.ts
+++ src/app/api/api.ts
@@ -1,8 +1,43 @@
 import GhostContentAPI from "@tryghost/content-api";
+import axios from "axios";
+import https from "https";
 
 // Create API instance with site credentials
+// export const api = new GhostContentAPI({
+//   url: 'https://localhost:2368',
+//   key: 'f7c46eb29fea49b966314a242f',
+//   version: "v6.0"
+// });
+
+
+//new
+// export const api = new GhostContentAPI({
+//   url: 'https://localhost',
+//   key: '37a973cc1bc7af641c21d8b2f9',
+//   version: "v6.0"
+// });
+
+
 export const api = new GhostContentAPI({
-  url: 'http://localhost:2368',
-  key: 'f7c46eb29fea49b966314a242f',
-  version: "v6.0"
+  url: 'https://localhost',
+  key: '37a973cc1bc7af641c21d8b2f9',
+  version: "v6.0",
+  makeRequest: ({ url, method, params, headers }) => {
+    const fn = axios[method as "get" | "post" | "put" | "delete"] || axios.get;
+    return fn(url, {
+      params,
+      paramsSerializer: (parameters: any) => {
+        return Object.keys(parameters).reduce((parts: string[], k: string) => {
+          const val = encodeURIComponent(([].concat(parameters[k]) as string[]).join(','));
+          return parts.concat(`${k}=${val}`);
+        }, []).join('&');
+      },
+      headers,
+      httpsAgent: new https.Agent({
+        rejectUnauthorized: false
+      })
+    });
+  }
 });
+
+//admin api key - 6a57185fdb1915000148334f:65d7b06f3d0325930f1bbf83d0a1021615424c008503194317e655772c1f1b24
