93bc76386988c826a3bcd83cf688b86e42976ef5
Ashan Dimantha <Ashan Dimantha>
2026-08-06 15:24:28 +0530

    chore: update home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,22 @@
-import Link from 'next/link'
+import Link from "next/link";
 
 export default function page() {
   return (
-    <div>
+    <div className="m-8">
       <h1>Welcome to NextJs - CMS Integrations</h1>
-      <h2>Ghost | Payload CMS | Strapi</h2>
-      <br />
-      <Link href="/blog" className='bg-blue-900 p-3 rounded-sm'>Visit Blogs Page</Link>
+      <h2 className="mb-8 font-bold">Ghost Headless</h2>
+
+      <Link href="/news" className="bg-green-400 p-3 rounded-sm mr-2">
+        News
+      </Link>
+
+      <Link href="/news" className="bg-amber-300 p-3 rounded-sm mr-2">
+        CSR Projects
+      </Link>
+
+      <Link href="/blog" className="bg-blue-900 p-3 rounded-sm mr-2">
+        Visit All Page
+      </Link>
     </div>
   );
 }
